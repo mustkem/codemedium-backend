4b8dbe33c5090a56f3e53d86d878ef7dccf3d404
mustkem <mo.mustkem@gmail.com>
2021-02-24 15:19:34 +0530

    add

--- admin/controllers/feed.js
+++ admin/controllers/feed.js
@@ -7,22 +7,19 @@ const Post = require('../models/post');
 const User = require('../models/adminUser');
 
 exports.getPosts = (req, res, next) => {
-  const currentPage = req.query.page || 1;
-  const perPage = 2;
-  let totalItems;
-  Post.find()
-    .countDocuments()
-    .then(count => {
-      totalItems = count;
-      return Post.find()
-        .skip((currentPage - 1) * perPage)
-        .limit(perPage);
+
+  const userId = req.userId;
+
+  User.findById(userId)
+    .populate("posts")
+    .then(user => {
+      return user
     })
-    .then(posts => {
+    .then(user => {
+      const posts = user.posts;
       res.status(200).json({
         message: 'Fetched posts successfully.',
         posts: posts,
-        totalItems: totalItems
       });
     })
     .catch(err => {
