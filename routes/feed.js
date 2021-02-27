32658427fe0f3751297ffb52b7277da33d6b0b69
mustkem <mo.mustkem@gmail.com>
2021-02-27 15:14:33 +0530

    add

--- routes/feed.js
+++ routes/feed.js
@@ -11,37 +11,41 @@ router.get('/posts',
 // isAuth, 
 feedController.getPosts);
 
-// POST /feed/post
-router.post(
-  '/post',
-  isAuth,
-  [
-    body('title')
-      .trim()
-      .isLength({ min: 5 }),
-    body('content')
-      .trim()
-      .isLength({ min: 5 })
-  ],
-  feedController.createPost
-);
 
-router.get('/post/:slug', isAuth, feedController.getPost);
 
-router.put(
-  '/post/:postId',
-  isAuth,
-  [
-    body('title')
-      .trim()
-      .isLength({ min: 5 }),
-    body('content')
-      .trim()
-      .isLength({ min: 5 })
-  ],
-  feedController.updatePost
-);
+router.get('/post/:slug', 
+// isAuth, 
+feedController.getPost);
 
-router.delete('/post/:postId', isAuth, feedController.deletePost);
+// router.put(
+//   '/post/:postId',
+//   isAuth,
+//   [
+//     body('title')
+//       .trim()
+//       .isLength({ min: 5 }),
+//     body('content')
+//       .trim()
+//       .isLength({ min: 5 })
+//   ],
+//   feedController.updatePost
+// );
+
+// // POST /feed/post
+// router.post(
+//   '/post',
+//   isAuth,
+//   [
+//     body('title')
+//       .trim()
+//       .isLength({ min: 5 }),
+//     body('content')
+//       .trim()
+//       .isLength({ min: 5 })
+//   ],
+//   feedController.createPost
+// );
+
+// router.delete('/post/:postId', isAuth, feedController.deletePost);
 
 module.exports = router;
